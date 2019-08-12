bf866bf6624c7b4df6b169da4100c8709f854f72
Joe Keenan <joe@flyingdiver.com>
2019-08-12 08:34:23 -0400

    Updates
    Added Status Update control to Switch (relay) device.
    Made JSON default for state location.
    
    general cleanup.

--- Shims.indigoPlugin/Contents/Server Plugin/plugin.py
+++ Shims.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -26,7 +26,7 @@ class Plugin(indigo.PluginBase):
         except:
             self.logLevel = logging.INFO
         self.indigo_log_handler.setLevel(self.logLevel)
-        self.logger.debug(u"logLevel = " + str(self.logLevel))
+        self.logger.threaddebug(u"logLevel = " + str(self.logLevel))
 
 
     def startup(self):
@@ -37,28 +37,6 @@ class Plugin(indigo.PluginBase):
     def shutdown(self):
         indigo.server.log(u"Shutting down Shims")
 
-
-    def message_handler(self, notification):
-        self.logger.debug(u"received notification of MQTT message type {} from device {}".format(notification["message_type"], notification["brokerID"]))
-
-        mqttPlugin = indigo.server.getPlugin("com.flyingdiver.indigoplugin.mqtt")
-        if not mqttPlugin.isEnabled():
-            self.logger.error(u"MQTT plugin not enabled, message_handler aborting.")
-            return
-        
-        message_data = None
-        for deviceID in self.shimDevices:
-            device = indigo.devices[deviceID]
-            message_type = device.pluginProps['message_type']
-            if notification["message_type"] == message_type:
-                if not message_data:
-                    props = { 'message_type': message_type }
-                    brokerID =  int(device.pluginProps['brokerID'])
-                    message_data = mqttPlugin.executeAction("fetchQueuedMessage", deviceId=brokerID, props=props, waitUntilDone=True)
-                if message_data != None:
-                    self.logger.debug(u"{}: sending {} to matchAndUpdate: {}".format(device.name, message_type, message_data["payload"]))
-                    self.matchAndUpdate(device, message_data)
-                    
 
     def deviceStartComm(self, device):
         self.logger.info(u"{}: Starting Device".format(device.name))
@@ -86,64 +64,68 @@ class Plugin(indigo.PluginBase):
         self.shimDevices.remove(device.id)
 
     def didDeviceCommPropertyChange(self, oldDevice, newDevice):
-        return True
+        return False
 
-    def recurseDict(self, key_string, data_dict):
-        self.logger.threaddebug(u"recurseDict key_string = {}, data_dict= {}".format(key_string, data_dict))
-        try:
-            if '.' not in key_string:
-                if key_string[0] == '[':
-                    return data_dict[int(key_string[1:-1])]
-                else:
-                    return data_dict[key_string]
-            else:
-                split = key_string.split('.', 1)
-                self.logger.threaddebug(u"recurseDict split[0] = {}, split[1] = {}".format(split[0], split[1]))
-                if split[0][0] == '[':
-                    return self.recurseDict(split[1], data_dict[int(split[0][1:-1])])
-                else:
-                    return self.recurseDict(split[1], data_dict[split[0]])
-        except Exception as e:
-            self.logger.error(u"recurseDict error: {}".format(e))
-              
+
+    def message_handler(self, notification):
+        self.logger.debug(u"received notification of MQTT message type {} from {}".format(notification["message_type"], indigo.devices[int(notification["brokerID"])].name))
+
+        mqttPlugin = indigo.server.getPlugin("com.flyingdiver.indigoplugin.mqtt")
+        if not mqttPlugin.isEnabled():
+            self.logger.error(u"MQTT plugin not enabled, message_handler aborting.")
+            return
+        
+        message_data = None
+        for deviceID in self.shimDevices:
+            device = indigo.devices[deviceID]
+            message_type = device.pluginProps['message_type']
+            if notification["message_type"] == message_type:
+                if not message_data:
+                    props = { 'message_type': message_type }
+                    brokerID =  int(device.pluginProps['brokerID'])
+                    message_data = mqttPlugin.executeAction("fetchQueuedMessage", deviceId=brokerID, props=props, waitUntilDone=True)
+                if message_data != None:
+                    self.logger.debug(u"{}: message_handler: '{}' {} -> {}".format(device.name, message_type, '/'.join(message_data["topic_parts"]), message_data["payload"]))
+                    self.update(device, message_data)
+                    
     
-    def matchAndUpdate(self, device, message_data):    
+    def update(self, device, message_data):    
         try:
             if device.pluginProps['uid_location'] == "topic":
                 message_address = message_data["topic_parts"][int(device.pluginProps['uid_location_topic_field'])]
-                self.logger.debug(u"{}: matchAndUpdate topic message_address = {}".format(device.name, message_address))
+                self.logger.threaddebug(u"{}: update topic message_address = {}".format(device.name, message_address))
             elif device.pluginProps['uid_location'] == "payload":
                 payload = json.loads(message_data["payload"])
                 message_address = payload[device.pluginProps['uid_location_payload_key']]
-                self.logger.debug(u"{}: matchAndUpdate json message_address = {}".format(device.name, message_address))
+                self.logger.debug(u"{}: update json message_address = {}".format(device.name, message_address))
             else:
-                self.logger.debug(u"{}: matchAndUpdate can't determine address location".format(device.name))
+                self.logger.debug(u"{}: update can't determine address location".format(device.name))
                 return
         except Exception as e:
-            self.logger.error(u"{}: matchAndUpdate error determining device address field: {}".format(device.name, e))
+            self.logger.error(u"{}: update error determining device address field: {}".format(device.name, e))
             return
             
         if device.pluginProps['address'] != message_address:
-            self.logger.debug(u"{}: matchAndUpdate address mismatch: {} != {}".format(device.name, device.pluginProps['address'], message_address))
+            self.logger.debug(u"{}: update address mismatch: {} != {}".format(device.name, device.pluginProps['address'], message_address))
             return
             
         try:
             if device.pluginProps['state_location'] == "topic":
                 i = int(device.pluginProps['state_location_topic'])
-                self.logger.threaddebug(u"{}: matchAndUpdate state_location_topic = {}".format(device.name, i))
+                self.logger.threaddebug(u"{}: update state_location_topic = {}".format(device.name, i))
                 message_value = message_data["topic_parts"][i]
             elif (device.pluginProps['state_location'] == "payload") and (device.pluginProps['state_location_payload_type'] == "json"):
                 key = device.pluginProps['state_location_payload_key']
                 data = json.loads(message_data["payload"])
-                self.logger.threaddebug(u"{}: matchAndUpdate state_location_payload, key = {}".format(device.name, key))
+                self.logger.threaddebug(u"{}: update state_location_payload, key = {}".format(device.name, key))
                 message_value = self.recurseDict(key, data)
             elif (device.pluginProps['state_location'] == "payload") and (device.pluginProps['state_location_payload_type'] == "raw"):
                 message_value = message_data["payload"]
             else:
-                self.logger.debug(u"{}: matchAndUpdate can't determine value location".format(device.name))
+                self.logger.debug(u"{}: update can't determine value location".format(device.name))
             
         except Exception as e:
-            self.logger.error(u"{}: matchAndUpdate error determining message value: {}".format(device.name, e))
+            self.logger.error(u"{}: update error determining message value: {}".format(device.name, e))
             return
         
         if device.deviceTypeId in ["shimRelay", "shimOnOffSensor"]:
@@ -177,11 +159,11 @@ class Plugin(indigo.PluginBase):
             try:
                 value = float(message_value)
             except (TypeError, ValueError) as e:
-                self.logger.error(u"{}: matchAndUpdate unable to convert '{}' to float: {}".format(device.name, message_value, e))
+                self.logger.error(u"{}: update unable to convert '{}' to float: {}".format(device.name, message_value, e))
                 return
                            
             function = device.pluginProps.get("adjustmentFunction", None)            
-            self.logger.debug(u"{}: matchAndUpdate adjustmentFunction: '{}'".format(device.name, function))
+            self.logger.threaddebug(u"{}: update adjustmentFunction: '{}'".format(device.name, function))
             if function:
                 if 'indigo' in function:
                     self.logger.warning(u"{}: Invalid method in adjustmentFunction: '{}'".format(device.name, function))
@@ -243,13 +225,13 @@ class Plugin(indigo.PluginBase):
 
 
             else:
-                self.logger.debug(u"{}: matchAndUpdate, unknown shimSensorSubtype: {}".format(device.pluginProps["shimSensorSubtype"]))
+                self.logger.debug(u"{}: update, unknown shimSensorSubtype: {}".format(device.pluginProps["shimSensorSubtype"]))
                 
             states_key = device.pluginProps.get('state_dict_payload_key', None)
             if not states_key:
                 return
             data = json.loads(message_data["payload"])
-            self.logger.threaddebug(u"{}: matchAndUpdate state_dict_payload_key, key = {}".format(device.name, states_key))
+            self.logger.threaddebug(u"{}: update state_dict_payload_key, key = {}".format(device.name, states_key))
             states_dict = self.recurseDict(states_key, data)
             if not (states_dict and len(states_dict) > 0):
                return
@@ -261,7 +243,7 @@ class Plugin(indigo.PluginBase):
                 new_states.append(key)
                 states_list.append({'key': key, 'value': states_dict[key], 'decimalPlaces': 2})
             if old_states != new_states:
-                self.logger.threaddebug(u"{}: matchAndUpdate, new states_list: {}".format(device.name, new_states))
+                self.logger.threaddebug(u"{}: update, new states_list: {}".format(device.name, new_states))
                 newProps = device.pluginProps
                 newProps["states_list"] = new_states
                 device.replacePluginPropsOnServer(newProps)
@@ -270,6 +252,25 @@ class Plugin(indigo.PluginBase):
 
         else:
             self.logger.warning(u"{}: Invalid device type: {}".format(device.name, device.deviceTypeId))
+            
+    def recurseDict(self, key_string, data_dict):
+        self.logger.threaddebug(u"recurseDict key_string = {}, data_dict= {}".format(key_string, data_dict))
+        try:
+            if '.' not in key_string:
+                if key_string[0] == '[':
+                    return data_dict[int(key_string[1:-1])]
+                else:
+                    return data_dict[key_string]
+            else:
+                split = key_string.split('.', 1)
+                self.logger.threaddebug(u"recurseDict split[0] = {}, split[1] = {}".format(split[0], split[1]))
+                if split[0][0] == '[':
+                    return self.recurseDict(split[1], data_dict[int(split[0][1:-1])])
+                else:
+                    return self.recurseDict(split[1], data_dict[split[0]])
+        except Exception as e:
+            self.logger.error(u"recurseDict error: {}".format(e))
+              
 
     def getStateList(self, filter, valuesDict, typeId, deviceId):
         returnList = list()
@@ -306,7 +307,7 @@ class Plugin(indigo.PluginBase):
 
 
     ########################################
-    # Device Control methods
+    # Relay / Dimmer Action callback
     ########################################
 
     def actionControlDevice(self, action, device):
@@ -331,6 +332,26 @@ class Plugin(indigo.PluginBase):
 
         else:
             self.logger.error(u"{}: actionControlDevice: Unsupported action requested: {}".format(device.name, action.deviceAction))
+
+    ########################################
+    # General Action callback
+    ########################################
+
+    def actionControlUniversal(self, action, device):
+
+        action_template =  device.pluginProps.get("action_template", None)
+        if not action_template:
+            self.logger.error(u"{}: actionControlDevice: no action template".format(device.name))
+            return
+        topic = pystache.render(action_template, {'uniqueID': device.address})
+
+        if action.deviceAction == indigo.kUniversalAction.RequestStatus:
+            self.logger.debug(u"{}: actionControlUniversal: RequestStatus".format(device.name))
+            self.publish_topic(device, topic, "")
+
+        else:
+            self.logger.error(u"{}: actionControlUniversal: Unsupported action requested: {}".format(device.name, action.deviceAction))
+
 
     def publish_topic(self, device, topic, payload):
     
